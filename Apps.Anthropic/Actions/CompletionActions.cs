5e4f0724387179661dc506be17aa95d3bbd3f868
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-06-19 11:12:52 +0300

    Added glossary optional input

--- Apps.Anthropic/Actions/CompletionActions.cs
+++ Apps.Anthropic/Actions/CompletionActions.cs
@@ -1,26 +1,28 @@
-﻿using Apps.Anthropic.Api;
+﻿using System.Text;
+using Apps.Anthropic.Api;
 using Apps.Anthropic.Models.Request;
 using Apps.Anthropic.Models.Response;
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Actions;
+using Blackbird.Applications.Sdk.Common.Files;
 using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.SDK.Extensions.FileManagement.Interfaces;
+using Blackbird.Applications.Sdk.Glossaries.Utils.Converters;
 using RestSharp;
 
 namespace Apps.Anthropic.Actions;
 
 [ActionList]
-public class CompletionActions : BaseInvocable
+public class CompletionActions(InvocationContext invocationContext, IFileManagementClient fileManagementClient) : BaseInvocable(invocationContext)
 {
-    public CompletionActions(InvocationContext invocationContext) : base(invocationContext)
-    {
-    }
-    
-    [Action("Create message", Description = "Create a message")]
-    public async Task<ResponseMessage> CreateCompletion([ActionParameter] CompletionRequest input)
+    [Action("Chat", Description = "Create a message")]
+    public async Task<ResponseMessage> CreateCompletion([ActionParameter] CompletionRequest input,
+        [ActionParameter] GlossaryRequest glossaryRequest)
     {
         var client = new AnthropicRestClient(InvocationContext.AuthenticationCredentialsProviders);
         var request = new RestRequest("/messages", Method.Post);
-        var messages = new List<Message>() { new Message { Role = "user", Content = input.Prompt } };
+        var messages = await GenerateChatMessages(input, glossaryRequest);
+        
         request.AddJsonBody(new
         {
             system = input.SystemPrompt ?? "",
@@ -32,11 +34,53 @@ public class CompletionActions : BaseInvocable
             top_p = input.TopP != null ? float.Parse(input.TopP) : 1.0f,
             top_k = input.TopK != null ? input.TopK : 1,
         });
+        
         var response = await client.ExecuteWithErrorHandling<CompletionResponse>(request);
-
         return new ResponseMessage
         {
             Text = response.Content.FirstOrDefault()?.Text ?? ""
         };
     }    
+    
+    private async Task<List<Message>> GenerateChatMessages(CompletionRequest input, GlossaryRequest? glossaryRequest)
+    {
+        var messages = new List<Message>();
+
+        if (!string.IsNullOrEmpty(input.SystemPrompt))
+            messages.Add(new Message { Role = "system", Content = input.SystemPrompt });
+
+        messages.Add(new Message { Role = "user", Content = input.Prompt });
+
+        if (glossaryRequest?.Glossary != null)
+        {
+            var glossaryPromptPart = await GetGlossaryPromptPart(glossaryRequest.Glossary);
+            messages.Add(new Message { Role = "user", Content = $"Glossary: {glossaryPromptPart}" });
+        }
+
+        return messages;
+    }
+
+    private async Task<string> GetGlossaryPromptPart(FileReference glossary)
+    {
+        var glossaryStream = await fileManagementClient.DownloadAsync(glossary);
+        var blackbirdGlossary = await glossaryStream.ConvertFromTbx();
+
+        var glossaryPromptPart = new StringBuilder();
+        glossaryPromptPart.AppendLine();
+        glossaryPromptPart.AppendLine("Glossary entries (each entry includes terms in different languages. Each language may have a few synonymous variations which are separated by ;;):");
+
+        foreach (var entry in blackbirdGlossary.ConceptEntries)
+        {
+            glossaryPromptPart.AppendLine();
+            glossaryPromptPart.AppendLine("\tEntry:");
+
+            foreach (var section in entry.LanguageSections)
+            {
+                glossaryPromptPart.AppendLine(
+                    $"\t\t{section.LanguageCode}: {string.Join(";; ", section.Terms.Select(term => term.Term))}");
+            }
+        }
+
+        return glossaryPromptPart.ToString();
+    }
 }
---
+++ Apps.Anthropic/Models/Request/GlossaryRequest.cs
@@ -0,0 +1,8 @@
+﻿using Blackbird.Applications.Sdk.Common.Files;
+
+namespace Apps.Anthropic.Models.Request;
+
+public class GlossaryRequest
+{
+    public FileReference? Glossary { get; set; }
+}
