8a0005a6c1e7e0a32d662677aabdec869d7b6c21
mathijs-bb <mathijs.sonnemans@blackbird.io>
2024-06-26 11:27:48 +0200

    Updated spelling of Sonnet

--- Apps.Anthropic/DataSourceHandlers/EnumHandlers/ModelDataSourceHandler.cs
+++ Apps.Anthropic/DataSourceHandlers/EnumHandlers/ModelDataSourceHandler.cs
@@ -7,7 +7,7 @@ public class ModelDataSourceHandler : IStaticDataSourceHandler
     public Dictionary<string, string> GetData()
         => new()
         {
-            { "claude-3-5-sonnet-20240620", "Claude 3.5 Sonet" },
+            { "claude-3-5-sonnet-20240620", "Claude 3.5 Sonnet" },
             { "claude-3-opus-20240229", "Claude 3 Opus" },
             { "claude-3-sonnet-20240229", "Claude 3 Sonnet" },
             { "claude-3-haiku-20240307", "Claude 3 Haiku" },
